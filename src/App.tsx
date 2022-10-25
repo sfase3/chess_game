41980b62263d5ed23b877c967b44c6bda749d849
sfase3 <kovalevvanya50@gmail.com>
2022-10-26 01:42:04 +0300

    2nd part of my work

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import BoardComponent from './components/BoardComponent';
+import { Board } from './models/Board';
+
+function App() {
+  const[board,setBoard] = useState(new Board())
+
+  useEffect(()=>{
+restart()
+  },[])
+
+  function restart(){
+    const newBoard = new Board()
+    newBoard.initCells()
+    newBoard.addFigures()
+    setBoard(newBoard)
+  }
+
+  return (
+    <div className="app">
+   <BoardComponent board={board} setBoard={setBoard}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/BoardComponent.tsx
@@ -0,0 +1,38 @@
+import React, { FC, useState } from 'react'
+import { Board } from '../models/Board';
+import { Cell } from '../models/Cell';
+import CellComponent from './CellComponent';
+
+interface BoardProps{
+  board: Board;
+  setBoard: (board: Board) => void;
+}
+
+const BoardComponent: FC<BoardProps> = ({board,setBoard}) =>{
+  const [selectedCell,setSelectedCell] = useState<Cell | null>(null)
+  
+function click(cell: Cell){
+  setSelectedCell(cell);
+}
+
+  return (
+    <div className='board'>
+      {board.cells.map((row,index) =>
+      <React.Fragment key={index}>
+        {row.map(cell=>
+          <CellComponent
+           click={click}
+           cell={cell} 
+           key={cell.id}
+           selected = {cell.x === selectedCell?.x && cell.y === selectedCell?.y } 
+           />
+          )}
+      </React.Fragment>
+      )}
+        </div>
+  )
+}
+
+export default BoardComponent
+
+
